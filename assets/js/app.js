0e8cce52b7f52d3795e993676e5d3a0cfacb572c
Ted <p0599847@protonmail.com>
2022-05-06 23:25:51 +0200

    home corpus

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,16 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import "../css/app.scss";
+import { Dropdown } from "bootstrap";
+
+const enableDropdowns = () => {
+  var dropdownElementList = [].slice.call(
+    document.querySelectorAll(".dropdown-toggle")
+  );
+  var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+    return new Dropdown(dropdownToggleEl);
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  enableDropdowns();
+});
